45c9760dcde6f66b6caa7b861c415a77133d7aef
DETA SARI ASLINA <112565733+Detasariaslina09@users.noreply.github.com>
2022-11-10 10:24:14 +0700

    Add files via upload

---
+++ DETA SARI ASLINA KE-2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<ncurses\ncurses.h>
+#include<windows.h>
+using namespace std;
+int main(){
+	//DETA SARI ASLINA
+	//2217051117
+	//KELAS D (POST TEST)
+	system("color E0");
+	int i,j;
+	for(i=0;i<10;i++){
+	for(j=0;j<10;j++)
+	printf("\n\t\t\t\t\tL O A D I N G . . . ");
+	Sleep(100);
+	}
+	system("color F0");
+	printf("\n");
+    for(int i=0;i<=100;i++){
+    cout<<endl<<"\n\n\n\t\t\t\t\tL O A D I N G . . . "<<i<<"%"<<endl;
+    system("cls");
+    }
+	cout<<"\n\n\n\t\t\t\t\t......... postest ddp ........."<<endl;
+    cout<<"\n\n\n\t\t\t\t\t................................"<<endl;
+    cout<<"\n\n\n\t\t\t\t\t................................"<<endl;
+	cout<<"\n\n\n\t\t\t\t\t........LOADING COMPLETE........"<<endl;
+	cout<<"\n\n\n\t\t\t\t\t................................"<<endl;
+    cout<<"\n\n\n\t\t\t\t\t................................"<<endl;
+    cout<<"\n\n\n\t\t\t\t\t.........detasariaslina.........";
+    system("pause>0");
+    return 0;
+}
